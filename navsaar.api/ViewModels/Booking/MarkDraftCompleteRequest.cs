6024fb7dd9443fcf616c0303ba68708c4fbef476
diya jain <diyajain711@gmail.com>
2026-03-13 12:04:43 +0530

    file is not mandatory while mark complete draft and allotment

--- navsaar.api/ViewModels/Booking/MarkDraftCompleteRequest.cs
+++ navsaar.api/ViewModels/Booking/MarkDraftCompleteRequest.cs
@@ -4,7 +4,7 @@
     {
         public int  Id { get; set; }
         public int UserId { get; set; }
-        public IFormFile File { get; set; }
+        public IFormFile? File { get; set; }
         public string Notes { get; set; }
     }
 }
